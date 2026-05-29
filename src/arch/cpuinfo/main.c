7e1bf227ae31161f2fbbd03e57c0cf133747c680
Mystic <frankbuider3@gmail.com>
2026-05-29 10:21:43 -0400

    Update main.c

--- src/arch/cpuinfo/main.c
+++ src/arch/cpuinfo/main.c
@@ -1,5 +1,5 @@
-#include <stdint.h>
-
+#include <stdint.h> 
+// I use arch btw
 void cpuid(uint32_t leaf, uint32_t* eax, uint32_t* ebx, uint32_t* ecx, uint32_t* edx) {
     __asm__ volatile (
         "cpuid"
